09654d83019fd354473a4b53fc233c9c43315590
Denis Bugayov <denis.bugayov@gmail.com>
2016-06-18 20:46:19 +0400

    Добавлен первый слайд с вертикальной и горизонтальной разметкой.

--- app/src/main/java/com/speedsumm/bu/aboutmegb/ProbaFragment.java
+++ app/src/main/java/com/speedsumm/bu/aboutmegb/ProbaFragment.java
@@ -1,10 +1,12 @@
 package com.speedsumm.bu.aboutmegb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 /**
  * Created by bu on 15.06.2016.
@@ -13,9 +15,15 @@ public class ProbaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment, container, false);
 
+
+
+
         return rootView;
+
     }
+
 }
